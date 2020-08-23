3af091eff3318a007ff0cffc1964ca0d86d5ab82
CodeWithGamez <codewithgamez@gmail.com>
2020-08-23 17:54:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Comment my code of STRING CONCATENATION
+
+
+#1 
+
+print( "Hello" + " world" + " was one of the first things I printed")
+
+
+
+#2
+
+words_1 = "I am getting better"
+words_2 = " as I keep practice"
+print(words_1 + words_2)
+
+
+#3
+
+words_3 = "I would recommend "
+words_4 = "practice everyday"
+number = 15
+words_5 = "minutes a day minimum if possible. "
+
+print(words_3 + words_4 + " " + str(number) + " " + words_5)
+
